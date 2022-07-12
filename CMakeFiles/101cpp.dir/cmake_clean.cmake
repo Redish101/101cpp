8b84e76d527fbe256625648049126ab302c6798b
Redish101 <jiayunluo@outlook.com>
2022-07-12 13:59:45 +0800

    init

---
+++ CMakeFiles/101cpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "101cpp"
+  "101cpp.pdb"
+  "CMakeFiles/101cpp.dir/main.cpp.o"
+  "CMakeFiles/101cpp.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/101cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
